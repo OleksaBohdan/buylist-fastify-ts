663a513ab18009aba9680cf076af80be5f650a8b
OleksaBohdan <bohdan.oleksa@gmail.com>
2022-12-04 13:52:41 +0100

    continue create frontend on vue

--- src/public/static/js/app.js
+++ src/public/static/js/app.js
@@ -1,7 +1,7 @@
 const App = {
   data() {
     return {
-      appTitle: 'Menulist:',
+      appTitle: 'Buylist:',
       placeholderProduct: 'Product name',
       placeholderProductCount: 'Count',
       productValue: '',
@@ -9,26 +9,44 @@ const App = {
       products: [
         { productName: 'Banana', productCount: '3', isNotDone: true },
         { productName: 'Tomato', productCount: '1', isNotDone: true },
+        { productName: 'Orange', isNotDone: true },
       ],
     };
   },
   methods: {
-    productChangeHadler(e) {
-      this.productValue = e.target.value;
-    },
+    // productChangeHadler(e) {
+    //   this.productValue = e.target.value;
+    // },
     productCountHandler(e) {
       this.productCountValue = e.target.value;
     },
     addProduct(e) {
-      const productName = this.productValue;
-      const productCount = this.productCountValue;
-      this.products.push({ productName: productName, productCount: productCount, isNotDone: true });
-      this.productValue = '';
-      this.productCountValue = '';
+      if (this.productValue !== '') {
+        const productName = this.productValue;
+        const productCount = this.productCountValue;
+        this.products.push({ productName: productName, productCount: productCount, isNotDone: true });
+        this.productValue = '';
+        this.productCountValue = '';
+      }
     },
-    doneProduct(e) {},
+    doneProduct(idx) {
+      if (this.products[idx].isNotDone == true) {
+        this.products[idx].isNotDone = false;
+      } else {
+        this.products[idx].isNotDone = true;
+      }
+    },
     deleteProducts(e) {
       this.products = [];
+    },
+    toUpperCase(item) {
+      return item.toUpperCase();
+    },
+  },
+  computed: {},
+  watch: {
+    productValue(value) {
+      console.log(value);
     },
   },
 };
